4f88d82a5d83d12817581a7a2cd18164f29156b3
theic <ic.icreator@gmail.com>
2022-03-23 16:09:10 +0200

    added random generator

--- dist/js/StarPort.js
+++ dist/js/StarPort.js
@@ -3,9 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.StarPort = void 0;
 const SpaceXRocket_1 = require("./SpaceXRocket");
 const MilitaryRocket_1 = require("./MilitaryRocket");
+const NasaRocket_1 = require("./NasaRocket");
 class StarPort {
     getAllRockets() {
-        return [new SpaceXRocket_1.SpaceXRocket(), new MilitaryRocket_1.MilitaryRocket()];
+        const rocketTypes = new Array();
+        rocketTypes.push(SpaceXRocket_1.SpaceXRocket, NasaRocket_1.NasaRocket, MilitaryRocket_1.MilitaryRocket);
+        const result = [];
+        let amount = 10;
+        while (amount > 0) {
+            const rand = Math.floor(Math.random() * 3);
+            result.push(new rocketTypes[rand]);
+            amount--;
+        }
+        return result;
     }
 }
 exports.StarPort = StarPort;
--- src/scripts/StarPort.ts
+++ src/scripts/StarPort.ts
@@ -1,9 +1,23 @@
 import { RocketBase } from './RocketBase';
 import { SpaceXRocket } from './SpaceXRocket';
 import { MilitaryRocket } from './MilitaryRocket';
+import { NasaRocket } from './NasaRocket';
 
 export class StarPort {
     getAllRockets(): RocketBase[] {
-        return [new SpaceXRocket(), new MilitaryRocket()];
+
+        const rocketTypes = new Array<typeof SpaceXRocket | typeof NasaRocket | typeof MilitaryRocket>();
+        rocketTypes.push(SpaceXRocket, NasaRocket, MilitaryRocket);
+
+        const result: RocketBase[] = [];
+        let amount = 10;
+
+        while (amount > 0) {
+            const rand = Math.floor(Math.random() * 3);
+            result.push(new rocketTypes[rand]);
+            amount--;
+        }
+
+        return result;
     }
 }
